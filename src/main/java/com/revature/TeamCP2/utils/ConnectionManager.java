1c7a761251540038e6c32f90292d875a3ecf0575
Tony Henderson <th_rams94@hotmail.com>
2022-05-05 18:53:58 -0500

    Initial commit

---
+++ src/main/java/com/revature/TeamCP2/utils/ConnectionManager.java
@@ -0,0 +1,52 @@
+package com.revature.TeamCP2.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+    private SessionFactory sf;
+    private Session session;
+    private List<Class> annotatedEntities;
+
+
+    private ConnectionManager(){
+        annotatedEntities = new LinkedList<>();
+    }
+
+    public static ConnectionManager getConnection(){
+        if(connectionManager == null){
+            connectionManager = new ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    public void initializeDatasource(){
+        Configuration config = new Configuration();
+
+        for(Class c : annotatedEntities){
+            config.addAnnotatedClass(c);
+        }
+
+        sf = config.buildSessionFactory();
+        session = sf.openSession();
+    }
+
+    public ConnectionManager addAnnotatedClass(Class c){
+        annotatedEntities.add(c);
+        return this;
+    }
+
+    public SessionFactory getSf() {
+        return sf;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+}
