c9c10f02fc8425f6d6f23da2136a0929d3bef5f9
Brandon Le <61598437+note084@users.noreply.github.com>
2022-05-13 07:39:27 -0700

    finished drafted version of UserService implementation

--- src/main/java/com/revature/TeamCP2/beans/services/UserService.java
+++ src/main/java/com/revature/TeamCP2/beans/services/UserService.java
@@ -7,9 +7,13 @@
 
 package com.revature.TeamCP2.beans.services;
 
+import com.revature.TeamCP2.beans.repositories.PaymentRepository;
+import com.revature.TeamCP2.beans.repositories.UserAddressRepository;
 import com.revature.TeamCP2.beans.repositories.UserRepository;
 import com.revature.TeamCP2.dtos.LoginDto;
+import com.revature.TeamCP2.entities.Payment;
 import com.revature.TeamCP2.entities.User;
+import com.revature.TeamCP2.entities.UserAddress;
 import com.revature.TeamCP2.exceptions.*;
 import com.revature.TeamCP2.interfaces.Role;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +26,16 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptHash bCrypt;
     private final AuthService authService;
+    private final UserAddressRepository userAddressRepository;
+    private final PaymentRepository paymentRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptHash bCrypt, AuthService authService) {
+    public UserService(UserRepository userRepository, BCryptHash bCrypt, AuthService authService, UserAddressRepository userAddressRepository, PaymentRepository paymentRepository) {
         this.userRepository = userRepository;
         this.authService = authService;
         this.bCrypt = bCrypt;
+        this.userAddressRepository = userAddressRepository;
+        this.paymentRepository = paymentRepository;
     }
 
     public User create(User user) throws CreationFailedException, ItemHasNonNullIdException, UsernameAlreadyExistsException {
@@ -47,10 +55,45 @@ public class UserService {
 
         return authService.loginUser(user);
     }
+
+    public User getByUsername(String username) {
+        return userRepository.getByUsername(username).get();
+    }
+
+    //needs a bit more logic regarding exceptions
+    public User updatePassword (User user, String currentPassword, String newPassword) throws ItemDoesNotExistException, UpdateFailedException, ItemHasNoIdException {
+        if (user.getPassword().equals(currentPassword)) {
+            user.setPassword(newPassword);
+            return userRepository.update(user);
+        }
+        else {
+            return null;
+        }
+    }
+
+    public User createUserAddress(User user, UserAddress address) throws ItemDoesNotExistException, UpdateFailedException, ItemHasNoIdException {
+        user.setUserAddresses(userAddressRepository.create(address));
+        return userRepository.update(user);
+    }
+
+    public User createUserPayment(User user, Payment payment) {
+        payment.setUser(user);
+        paymentRepository.create(payment);
+        return user;
+    }
+
+    public User updateUserAddress(User user, UserAddress address) throws ItemDoesNotExistException, UpdateFailedException, ItemHasNoIdException {
+        user.setUserAddresses(userAddressRepository.update(address));
+        return userRepository.update(user);
+    }
+
+    public User updateUserPayment(Payment payment) {
+        paymentRepository.update(payment);
+        return payment.getUser();
+    }
+
     public List<User> getAllUsers() {
-
-
-    return userRepository.getAll();
+        return userRepository.getAll();
     }
 
     public Optional<User> getById(int id) throws ItemDoesNotExistException {
