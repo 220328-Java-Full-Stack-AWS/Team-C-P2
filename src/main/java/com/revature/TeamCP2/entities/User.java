bac70314b0e508e2b7123ada17e590ab396c7133
Tony Henderson <th_rams94@hotmail.com>
2022-05-16 23:11:54 -0500

    Created/Implemented AuthServiceTest

--- src/main/java/com/revature/TeamCP2/entities/User.java
+++ src/main/java/com/revature/TeamCP2/entities/User.java
@@ -95,7 +95,7 @@ public class User implements Entity {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -192,42 +192,42 @@ public class User implements Entity {
     public void setDateModifies(Date dateModifies) {
         this.dateModifies = dateModifies;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (username != null ? !username.equals(user.username) : user.username != null) return false;
-        if (password != null ? !password.equals(user.password) : user.password != null) return false;
-        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
-        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
-        if (email != null ? !email.equals(user.email) : user.email != null) return false;
-//        if (addressId != null ? !addressId.equals(user.addressId) : user.addressId != null) return false;
-//        if (phone != null ? !phone.equals(user.phone) : user.phone != null) return false;
-//        if (paymentId != null ? !paymentId.equals(user.paymentId) : user.paymentId != null) return false;
-        if (dateCreated != null ? !dateCreated.equals(user.dateCreated) : user.dateCreated != null) return false;
-        if (dateModifies != null ? !dateModifies.equals(user.dateModifies) : user.dateModifies != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (username != null ? username.hashCode() : 0);
-        result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-//        result = 31 * result + (addressId != null ? addressId.hashCode() : 0);
-//        result = 31 * result + (phone != null ? phone.hashCode() : 0);
-//        result = 31 * result + (paymentId != null ? paymentId.hashCode() : 0);
-        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
-        result = 31 * result + (dateModifies != null ? dateModifies.hashCode() : 0);
-        return result;
-    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        User user = (User) o;
+//
+//        if (id != user.id) return false;
+//        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+//        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+//        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+//        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+//        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+////        if (addressId != null ? !addressId.equals(user.addressId) : user.addressId != null) return false;
+////        if (phone != null ? !phone.equals(user.phone) : user.phone != null) return false;
+////        if (paymentId != null ? !paymentId.equals(user.paymentId) : user.paymentId != null) return false;
+//        if (dateCreated != null ? !dateCreated.equals(user.dateCreated) : user.dateCreated != null) return false;
+//        if (dateModifies != null ? !dateModifies.equals(user.dateModifies) : user.dateModifies != null) return false;
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = id;
+//        result = 31 * result + (username != null ? username.hashCode() : 0);
+//        result = 31 * result + (password != null ? password.hashCode() : 0);
+//        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+//        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+//        result = 31 * result + (email != null ? email.hashCode() : 0);
+////        result = 31 * result + (addressId != null ? addressId.hashCode() : 0);
+////        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+////        result = 31 * result + (paymentId != null ? paymentId.hashCode() : 0);
+//        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
+//        result = 31 * result + (dateModifies != null ? dateModifies.hashCode() : 0);
+//        return result;
+//    }
 }
