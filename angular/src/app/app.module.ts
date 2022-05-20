eea59ad61286fc25cd641ac6e13309e693ba5c0a
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-05-19 22:56:24 -0500

    Added angular material

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BaseLayoutComponent } from './shared/components/base-layout/base-layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { LoginComponent } from './shared/components/login/login.component';
+import { RegisterComponent } from './shared/components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatStepperModule} from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -16,14 +21,18 @@ import { LoginComponent } from './shared/components/login/login.component';
     BaseLayoutComponent,
     HomeComponent,
     ProductsComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
