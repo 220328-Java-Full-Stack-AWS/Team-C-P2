12dcf9273a48097834c168d70beb1bc6f3608047
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-05-25 15:17:04 -0500

    Fixed Access-Control-Allow-Origin (can only contain one value)

--- src/main/java/com/revature/TeamCP2/beans/services/CorsFilter.java
+++ src/main/java/com/revature/TeamCP2/beans/services/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200, http://teamcp2-ui.s3-website-us-east-1.amazonaws.com/");
+        response.setHeader("Access-Control-Allow-Origin", "http://teamcp2-ui.s3-website-us-east-1.amazonaws.com/");
         response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
         filterChain.doFilter(request, response);
     }
