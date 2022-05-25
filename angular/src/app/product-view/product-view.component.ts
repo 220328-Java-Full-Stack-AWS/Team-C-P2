16bc34191d41823a74b0e7ce6e7a982c2158ecde
Diego Leon <diego042@revature.net>
2022-05-25 00:54:32 -0700

    Add to cart wotks on product view and products by category. Also,if not logged in add to cart button directs to login

--- angular/src/app/product-view/product-view.component.ts
+++ angular/src/app/product-view/product-view.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CartItem } from '../shared/interfaces/Cart-Interface/cart-item.interface';
 import { category } from '../shared/interfaces/Product-Interface/category.interface';
 import { onSale } from '../shared/interfaces/Product-Interface/onsale.interface';
+import { CookieService } from '../shared/services/cookie-service/cookie.service';
 import { ProductService } from '../shared/services/product-service/product.service';
 import { UserService } from '../shared/services/user-service/user.service';
 
@@ -14,6 +15,9 @@ import { UserService } from '../shared/services/user-service/user.service';
 export class ProductViewComponent implements OnInit {
   products: Item[] = [];
   quantity: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  isLoggedIn: boolean = false;
+
+  error?: string = "Error.";
 
   productById: Item = {
     netPrice: 0,
@@ -41,7 +45,11 @@ export class ProductViewComponent implements OnInit {
 
 
   id: any = "";
-  constructor(private productService: ProductService, private userService: UserService, private route: ActivatedRoute) { }
+  constructor(
+    private productService: ProductService
+    , private userService: UserService,
+    private route: ActivatedRoute,
+    private cookieService: CookieService) { }
   //
   ngOnInit(): void {
     // let id: any;
@@ -57,14 +65,41 @@ export class ProductViewComponent implements OnInit {
         }
       });
     });
+
+    if (this.cookieService.getCookie('user_session')) {
+      this.isLoggedIn = true;
+    }
+    else {
+      this.isLoggedIn = false;
+    }
   }
 
   addToCart(item: Item, event: Event, qty: any): void {
     event.stopPropagation();
-
     this.userService.addToCart(item.product, qty);
-
     (event.target as HTMLElement).classList.add('inCart');
+
+  }
+
+  // function to close error window
+  closeError(): void {
+    const el = document.querySelector(".error") as HTMLElement;
+
+    if (!el.classList.contains("hide")) {
+      el.classList.add("hide");
+      this.error = "";
+    }
+  }
+
+  // function to show errors
+  showError(errorMessage: string): void {
+    const el = document.querySelector('.error') as HTMLElement;
+
+    this.closeError();
+    if (el.classList.contains("hide")) {
+      this.error = errorMessage;
+      el.classList.remove("hide");
+    }
   }
 
 }
